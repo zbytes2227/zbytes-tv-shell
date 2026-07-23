25b740acf7926195548afe67064e826f218cfb35
zbytes2227 <zbytes0@gmail.com>
2026-07-23 13:05:47 +0530

    try to fix the window size

--- electron/main/windowManager.js
+++ electron/main/windowManager.js
@@ -2,47 +2,36 @@ const { BrowserWindow } = require("electron");
 const path = require("path");
 
 class WindowManager {
-
     constructor() {
         this.window = null;
     }
 
     create() {
-
         this.window = new BrowserWindow({
-
-            fullscreen: true,
-
-            autoHideMenuBar: true,
-
-            backgroundColor: "#000334",
-
-            show: false,
-
             titleBarStyle: "hidden",
-
+            frame: false,
+            fullscreen: true,
+            kiosk: true,
+            autoHideMenuBar: true,
+            show: false,
+            backgroundColor: "#000334",
             webPreferences: {
-
                 preload: path.join(__dirname, "../preload/preload.js"),
-
                 contextIsolation: true,
-
-                nodeIntegration: false
-
-            }
-
+                nodeIntegration: false,
+            },
         });
 
         this.window.once("ready-to-show", () => {
-
+            this.window.maximize();
+            this.window.setFullScreen(true);
+            this.window.setKiosk(true);
+            this.window.setMenuBarVisibility(false);
             this.window.show();
-
         });
 
         return this.window;
-
     }
-
 }
 
 module.exports = WindowManager;
